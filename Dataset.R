1cf88fc85a3f29ebab30d526c401c66d5d7c9249
Jacqueline <jacquelineliu24@gmail.com>
2019-02-18 12:07:26 +0100

    18 Feb 2019

--- Dataset.R
+++ Dataset.R
@@ -12,6 +12,7 @@ source("software/polygonizer.R")
 library(spex)
 library(dplyr)
 library(tidyverse)
+library(rgeos)
 
 # Load raster layers 
 treecover <- raster("gfc/treecover2000.tif")
@@ -36,8 +37,8 @@ prot <- st_read("Polygons/Jambi_1_GR_Tamonarang_Protection.shp")
 prod <- st_read("Polygons/Jambi_1_GR_Tamonarang_Production.shp")
 hud <- st_read("Polygons/Jambi_1_GR_Tamonarang_HD.shp")
 
-protection <- st_transform(prot, crs = 41001)
-hd <- st_transform(hud, crs = 41001)
+#protection <- st_transform(prot, crs = 41001)
+#hd <- st_transform(hud, crs = 41001)
 
 options(sf_max.plot=1)
 plot(st_geometry(jambi))
@@ -60,25 +61,96 @@ plot(st_geometry(jambi))
 plot(st_geometry(land_titles), add = TRUE)
 plot(land_titles_box, add = TRUE)
 
-
 # Crop raster layers to land_titles polygon
-crs(treecover) <- mercator
-crs(lossyear) <- mercator
-crs(gain) <- mercator
+lossyear_prot <- mask(lossyear, mask = prot)
+lossyear_prot_crop <- crop(lossyear_prot, prot)
+plot(lossyear_prot_crop)
 
-land_titles_sp <- as(land_titles, "Spatial")
-head(land_titles_sp)
-
-lossyear_tam <- mask(lossyear, mask = land_titles_sp)
-
-lossyear_prot <- mask(lossyear, mask = protection)
-lossyear_prot_crop <- crop(lossyear_prot, protection)
-
-lossyear_hd <- mask(lossyear, mask = hd)
-lossyear_hd_crop <- crop(lossyear_hd, hd)
+lossyear_hd <- mask(lossyear, mask = hud)
+lossyear_hd_crop <- crop(lossyear_hd, hud)
 
 lossyear_tam <- merge(lossyear_prot_crop, lossyear_hd_crop)
-
 plot(lossyear_tam)
-lossyear_tam_crop <- crop(lossyear_tam, land_titles)
 
+treecover_prot <- mask(treecover, mask = prot)
+treecover_prot_crop <- crop(treecover_prot, prot)
+
+treecover_hd <- mask(treecover, mask = hud)
+treecover_hd_crop <- crop(treecover_hd, hud)
+
+treecover_tam <- merge(treecover_prot_crop, treecover_hd_crop)
+plot(treecover_tam)
+
+gain_prot <- mask(gain, mask = prot)
+gain_prot_crop <- crop(gain_prot, prot)
+
+gain_hd <- mask(gain, mask = hud)
+gain_hd_crop <- crop(gain_hd, hud)
+
+gain_tam <- merge(gain_prot_crop, gain_hd_crop)
+plot(gain_tam)
+
+# Create raster stack
+crs(lossyear_tam) <- mercator
+crs(treecover_tam) <- mercator
+crs(gain_tam) <- mercator
+
+forestcover_tam = stack(treecover_tam, lossyear_tam, gain_tam)
+head(forestcover_tam)
+
+# Coerce raster to dataframe 
+forestcover_tam_df <- as.data.frame(forestcover_tam, xy = TRUE)
+sum(is.na(forestcover_tam_df))
+forestcover_tam_clean <- na.omit(forestcover_tam_df)
+sum(is.na(forestcover_tam_clean))
+head(forestcover_tam_clean)
+forestcover_tamonarang <- rename(forestcover_tam_clean, c("layer.1" = "treecover2000", "layer.2" = "lossyear", "layer.3" = "gain"))
+head(forestcover_tamonarang)
+
+# Save dataframe to drive
+saveRDS(forestcover_tamonarang, file = "tamonarang.Rda")
+tamonarang <- readRDS("tamonarang.Rda")
+head(tamonarang)
+
+# Convert dataframe to sf object
+# Changing projection to CRS = 4326 will allow us to plot the land title polygons on top of sf object
+tamonarang_sf <- st_as_sf(tamonarang, coords = c("x", "y"), crs = 4326)
+head(tamonarang_sf)
+
+plot(tamonarang_sf["treecover2000"], axes = TRUE)
+plot(tamonarang_sf["lossyear"], axes = TRUE)
+plot(tamonarang_sf["gain"], axes = TRUE)
+
+# Add column for Province
+tamonarang_sf$province <- c("Jambi")
+head(tamonarang_sf)
+
+plot(st_geometry(tamonarang_sf), col = "forest green", axes = TRUE) 
+plot(st_geometry(prot), border = "black", add = TRUE)
+plot(st_geometry(hud), border = "black", add = TRUE)
+
+tamonarang_sf$in_HD <- st_intersects(tamonarang_sf, hud, sparse = FALSE)
+head(tamonarang_sf)
+tamonarang_sf_1 <- tamonarang_sf[,-(6), drop = FALSE]
+head(tamonarang_sf_1)
+tamonarang_sf_1$forest_area_ENG <- c("Protection")
+tamonarang_sf_1$forest_area <- c("Hutan Lindung")
+head(tamonarang_sf_1)
+
+# Identifying the border between polygons 
+prot_sp <- as(prot, Class = "Spatial")
+hud_sp <- as(hud, Class = "Spatial")
+
+land_titles <- st_union(prot, hud)
+land_titles_sp <- as(land_titles, Class = "Spatial")
+
+border = gDifference(as(land_titles_sp, "SpatialLines"), as(gUnaryUnion(prot_sp, id = NULL), "SpatialLines"), byid = TRUE)
+border1 = gNearestPoints(as(prot_sp, "SpatialLines"), as(hud_sp, "SpatialLines"))
+head(border1)
+# Only returned 1 point - polygons may not actually be adjacent to each other? 
+
+# Checking the plots
+plot(st_geometry(tamonarang_sf), col = "forest green", axes = TRUE) 
+plot(st_geometry(prot), border = "black", add = TRUE)
+plot(st_geometry(hud), border = "black", add = TRUE)
+plot(border1, col = "red", lwd = 2, add = TRUE)
